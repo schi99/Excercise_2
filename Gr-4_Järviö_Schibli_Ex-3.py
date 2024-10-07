ac9d4d4f679178deecbc1890d728297ee12af2a5
schi99 <benjamin.schibli@gmail.com>
2024-10-07 18:45:13 +0200

    Added Test-Files (warm up)

--- Gr-4_Järviö_Schibli_Ex-3.py
+++ Gr-4_Järviö_Schibli_Ex-3.py
@@ -1,23 +1,6 @@
+# -*- coding: utf-8 -*-
 # Gr-4_Järviö_Schibli_Ex-2
 
 # Date: 02.10.2024
 
-# Warm Up / Guess and Check
-
-def square_1(x):
-    x: float    
-    solution = 0
-    while solution*solution < x:
-        solution = solution + 1
-        if solution*solution != (x:(x-0.1)):
-              while solution*solution < (x - 0.1):
-                  solution =+ 0.1
-        else:
-            print(f"{x} is the square root of" f"{x}")
-
-square_1( 27)
-
-    
-
-
-
+# Warm Up / See file test_square.py and test_eval_quadratic
