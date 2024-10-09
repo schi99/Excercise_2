d6dffe97fe4b705f46de5b03f09cc08f6f3df45e
schi99 <benjamin.schibli@gmail.com>
2024-10-09 19:43:58 +0200

    Finished Game (Needs Comments and good grammer)

--- ps3_hangman.py
+++ ps3_hangman.py
@@ -50,22 +50,11 @@ def is_word_guessed(secret_word, letters_guessed):
     returns: boolean, True if all the letters of secret_word are in letters_guessed;
       False otherwise
     '''
-    
-    secret_word = choose_word(wordlist).lower()
-    
-    letter_guessed += input()
 
-    index = 0
-
-    for i in range(len(secret_word)):
-      if secret_word[i] in letters_guessed:
-        index += 1
-        if index == len(secret_word):
-           return letters_guessed, secret_word, True
-      else:
-        return letters_guessed, secret_word, True
-
-
+    for i in secret_word:
+        if i not in letters_guessed:
+            return False
+    return True
 
 
 
@@ -76,14 +65,14 @@ def get_guessed_word(secret_word, letters_guessed):
     returns: string, comprised of letters and underscores that represents
       what letters in secret_word have been guessed so far.
     '''
-    guessed_word =  ""
+    guessed_word =  []
 
     for i in secret_word:
        if i in letters_guessed:
-          guessed_word += i
+          guessed_word.append(i)
        else:
-          guessed_word += "_"
-    return(guessed_word)
+          guessed_word.append("_")
+    return"".join(guessed_word)
 
 
 
@@ -93,7 +82,15 @@ def get_available_letters(letters_guessed):
     returns: string, comprised of letters that represents what letters have not
       yet been guessed.
     '''
-    # FILL IN YOUR CODE HERE...
+    import string
+    lowercase_letters = string.ascii_lowercase
+
+    available_letters = [i for i in lowercase_letters if i not in letters_guessed]
+    return''.join(available_letters)
+
+
+
+                    
     
 
 def hangman(secret_word):
@@ -102,30 +99,69 @@ def hangman(secret_word):
 
     Starts up an interactive game of Hangman.
 
+    '''
+
+    '''
     * At the start of the game, let the user know how many 
       letters the secret_word contains.
-
+    
     * Ask the user to supply one guess (i.e. letter) per round.
 
     * The user should receive feedback immediately after each guess 
       about whether their guess appears in the computers word.
-
+    
     * After each round, you should also display to the user the 
       partially guessed word so far, as well as letters that the 
       user has not yet guessed.
 
     Follows the other limitations detailed in the problem write-up.
     '''
-    # FILL IN YOUR CODE HERE...
+
+    letters_guessed = []
+    guesses = 0
+    limit = 8
+
+
+    print(f"Hello! Guess the secret word before you tries run out. Your word has {len(secret_word)} letters")
+
+    while guesses < limit + 1:
+
+      #### Ending the Game: ####
+
+      #Winning
+
+      if is_word_guessed(secret_word, letters_guessed) == True:
+        print(f"Wuuh - you won the word is {secret_word}")
+        break
+
+      #Loosing
+
+      else:
+        if guesses == limit:
+          print(f"Oh no you lost :( the word woulf have been {secret_word}")
+          break
+
+        else:
+           guess = input("Enter a letter you want to guess,").lower()
+
+           if guess in letters_guessed:
+            print(f"You allready tried letter {guess} , choos anotherone")
+            continue
+
+           else:
+            letters_guessed.append(guess)
+
+            progress = get_guessed_word(secret_word, letters_guessed)
+            print(f"This is the progress you made so far: {progress}")
+
+            available_letters = get_available_letters(letters_guessed)
+            print(f"You can choose one of the follwing letters: {available_letters}")
+            guesses += 1
+            print(f"You have {limit - guesses} left")
+       
 
 
 
 
-
-
-# When you've completed your hangman function, uncomment these two lines
-# and run this file to test! (hint: you might want to pick your own
-# secret_word while you're testing)
-
-# secret_word = choose_word(wordlist).lower()
-# hangman(secret_word) 
+secret_word = choose_word(wordlist).lower()
+hangman(secret_word) 
