a21f04a2d87aa624be1c483395b689259493b50a
Heini Järviö <heinimjarv@gmail.com>
2024-10-09 16:06:12 +0200

    Simplified

--- # Gr-4_Järviö_Schibli_Ex-2.py
+++ # Gr-4_Järviö_Schibli_Ex-2.py
@@ -1,5 +1,7 @@
 # Gr-4_Järviö_Schibli_Ex-2
 """
+Task 1:
+
 Calculating the credit card balance after one year with minimum monthly payments 
 with the given annual and monthly payment rates and 12 months of payments
 """
@@ -8,17 +10,26 @@ annual_interest_rate = 0.2
 monthly_payment_rate = 0.04
 payment_months = 12
 
-for i in range(1, payment_months + 1):  #adding 1 to include the 12th month
-    if i == 1: # because of the variable names the code for the first month is different
+for month in range(1, payment_months + 1):  #adding 1 to include the 12th month
         min_payment = balance * monthly_payment_rate
         remaining = balance - min_payment
-        balance_new = remaining + (remaining*(annual_interest_rate/12))
-    else:
-        min_payment = balance_new * monthly_payment_rate
-        remaining = balance_new - min_payment
-        balance_new = remaining + (remaining*(annual_interest_rate/12))
+        balance = remaining + (remaining*(annual_interest_rate/12))
 
-print("Remaining balance: ", format(balance_new, '.2f')) #formating the result to 2 decimals
+print("Remaining balance: ", format(balance, '.2f')) #formating the result to 2 decimals
+
+"""
+Task 2:
+
+Paying debt off in a year; finding the minimum fixed payment rate 
+with a given annual interest rate
+
+"""
+
+balance = 484
+annual_interest_rate = 0.2
+payment_months = 12
+
+monthly_payment = 0 
 
 
 
